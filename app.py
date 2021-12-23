7fe405bd726bd0ede76b30f43ad92b50f43daa35
Mondongo-140 <93956084+Mondongo-140@users.noreply.github.com>
2021-12-23 18:53:44 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def hello():
+	return "comeram meu cu"
+@app.route("/test", methods=["GET"])
+def test():
+	return "pika grossa"
+
+
+if __name__ == "__main__":
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host="0.0.0.0", port=port)
