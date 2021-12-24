48dff4db16487ab12d8a68a25768b8e198189709
Mondongo-140 <93956084+Mondongo-140@users.noreply.github.com>
2021-12-23 21:20:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def hello():
-	return "comeram meu cu"
+	return "tio Teles comeu seu cu"
 @app.route("/test", methods=["GET"])
 def test():
 	return "pika grossa"
